e7472a7f4452aca8d7a3faf7c4b47cd96a5b3d9a
Filippo Castelli <37574218+filippocastelli@users.noreply.github.com>
2019-01-22 19:06:33 +0100

    updated script

--- gaussianprocessregression/script_gpr.py
+++ gaussianprocessregression/script_gpr.py
@@ -11,7 +11,7 @@ from itertools import product
 #f = lambda x: np.cos(.7*x).flatten()
 
 N = 5     # numero punti training
-n = 500   # numero punti test
+n = 50   # numero punti test
 s = 0.0   # noise variance
 
 rng = np.random.RandomState(2)
@@ -29,14 +29,14 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.legend([cosine, measures], ["f(x)", "punti training"])
 plt.savefig('misure.png', bbox_inches='tight')
-#%%
+
 gen_data(x, x_guess,y, GPR.kernel_gaussian, R = 0.1, save = "gendata")
 #%%
 # PLOT PRIORI
-
+kernel = GPR.generate_kernel(GPR.kernel_gaussian, const = 0.1, length = 5 )
 prior(x = x,
       x_guess = x_guess,
-      kernel = GPR.kernel_gaussian)
+      kernel = kernel, R =10)
 #%%
 # PLOT POSTERIORI
 post(x = x,
@@ -50,6 +50,7 @@ create_case(x = x,
             y= y,
             x_guess = x_guess,
             kernel = GPR.kernel_gaussian,
+            R = 0.001,
             title = "GPR",
             save = "GPR",
             orig_function = True)
@@ -75,7 +76,9 @@ for i, r in enumerate([0.00001, 0.02, 0.1, 0.8, 1.5, 5.0]):
     create_case(x,
                 x_guess,
                 y,
-                GPR.generate_kernel(GPR.kernel_gaussian, length=noiseplot_length),
+                GPR.generate_kernel(GPR.kernel_gaussian,
+                                    const = 1,
+                                    length=noiseplot_length),
                 R=r)
     
 plt.savefig('noise_plot.png',bbox_inches='tight')
@@ -89,7 +92,9 @@ for i, l in enumerate([0.05, 0.5, 1, 3.2, 5.0, 7.0]):
     create_case(x,
                 x_guess,
                 y,
-                GPR.generate_kernel(GPR.kernel_gaussian, length=l),
+                GPR.generate_kernel(GPR.kernel_gaussian,
+                                    const = 1,
+                                    length=l),
                 R=lengthplot_noise)
     
 plt.savefig('length_plot.png', bbox_inches='tight')
@@ -98,12 +103,17 @@ plt.savefig('length_plot.png', bbox_inches='tight')
 #%% PARAMETER OPTIMIZATION
 
 gaus = GPR(x, y)
-R_list = np.linspace(0.0, 1, 100)
-L_list = np.linspace(0.1, 100, 100)
-lml, r_opt, l_opt= gaus.optimize3(R_list, L_list)
 
-print("best parameters (probability, noise, length_scale):",lml.max(),r_opt, l_opt)
+paramlist = {'length': np.linspace(0.1,100,100),
+             'const': np.linspace(0.1, 10, 10)}
 
-create_case(x, x_guess, y, kernel= GPR.generate_kernel(GPR.kernel_gaussian, length=l_opt), R=r_opt, title = "Parametri Otimizzati")
-plt.savefig('optimal_params.png', bbox_inches='tight')
+landscape, best_params = gaus.optimizer(param_dictionary=paramlist, noiselist = np.linspace(0.1,10,10))
 
+
+#%%
+
+#print("best parameters (probability, noise, length_scale):",lml.max(),r_opt, l_opt)
+#
+#create_case(x, x_guess, y, kernel= GPR.generate_kernel(GPR.kernel_gaussian, length=l_opt), R=r_opt, title = "Parametri Otimizzati")
+#plt.savefig('optimal_params.png', bbox_inches='tight')
+#
