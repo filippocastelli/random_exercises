859e42f08f588a251663517a64b70b4ca99ed2d7
Filippo Castelli <filippocastelli42@gmail.com>
2019-02-14 17:41:23 +0100

    updated script

--- GPR_REBOOT/script.py
+++ GPR_REBOOT/script.py
@@ -10,7 +10,7 @@ def f1(x):
 #f = lambda x: np.cos(.7*x).flatten()
 
 N = 5     # numero punti training
-n = 100   # numero punti test
+n = 1000   # numero punti test
 s = 0.0   # noise variance
 
 rng = np.random.RandomState(2)
@@ -36,9 +36,22 @@ gaus = GPR_reboot(x,y,
                 kernel_params = {'const': 1,
                                  'length': 1},
                 R =0.)
-
 #%%
-
+#predictor
 preds = gaus.predict()
 
-asse = gaus.plot(axlabels = ["x", "y"], save = "test_figure")
+gaus.plot(axlabels = ["x", "y"],title = 'before_optim', save = "test_figure")
+
+#%%
+optimizer_ranges = {'const': (0,10),
+                    'length': (0,10)}
+#
+optimized_params, logp, grid, grid_values = gaus.optimizer(ranges_dict = optimizer_ranges,
+                                                           mode = "asd",
+                                                           Ns = 500,
+                                                           output_grid = True)
+
+gaus.update_params(optimized_params)
+preds2 = gaus.predict()
+gaus.plot(title = "after optim",axlabels = ["x", "y"], save = "optim")
+
