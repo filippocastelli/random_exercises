4e4cd45e96bf019281bfad21487e6027c7410108
Filippo Castelli <37574218+filippocastelli@users.noreply.github.com>
2019-02-21 16:18:37 +0100

    disaster

--- GPR_REBOOT/GP_REBOOT.py
+++ GPR_REBOOT/GP_REBOOT.py
@@ -41,7 +41,6 @@ class GPR_reboot(object):
 
         self.kernel = kernel if kernel else self.gaussian_kernel
 
-        self.params = kernel_params
         if kernel_params == {}:
             warnings.warn(
                 "params for {} kernel not set!, remember to set them before using the predict method".format(
@@ -49,8 +48,7 @@ class GPR_reboot(object):
                 )
             )
         else:
-            self.calc_kernel_matrices()
-            self.kernel_setup()
+            self.update_params(kernel_params)
 
         self.kernel_arguments = self.find_arguments(self.kernel)
 
@@ -84,7 +82,7 @@ class GPR_reboot(object):
     # > KERNEL FUNCTION WRAPPING
     @classmethod
     def wrap_kernel(cls, kernel, **kwargs):
-
+        
         arguments = cls.find_arguments(kernel)
         argument_dict = kwargs
         arglist = tuple(argument_dict.keys())
@@ -113,7 +111,7 @@ class GPR_reboot(object):
         varnames = kernel.__code__.co_varnames
 
         try:
-            kernel_arguments = varnames[3 : varnames.index("const") + 1]
+            kernel_arguments = varnames[3 : varnames.index("wantgrad") + 1]
         except ValueError:
             raise Exception(f"kernel function {kernel} not valid")
 
@@ -126,8 +124,8 @@ class GPR_reboot(object):
         dim1 = len(data1)
         dim2 = len(data2)
 
-        dvec1 = np.tile(data1, (dim2, 1))
-        dvec2 = np.tile(data2, (dim1, 1)).T
+        dvec1 = np.tile(data1, (dim2, 1)).T
+        dvec2 = np.tile(data2, (dim1, 1))
 
         diff = dvec1 - dvec2
 
@@ -136,15 +134,25 @@ class GPR_reboot(object):
     # > KERNEL SETUP
     def kernel_setup(self):
         assert self.params != {}, "Kernel parameters not set!"
+        
+        # don't want kernels with enabled gradients to go into K, K* and K** calculations
+        if self.params['wantgrad'] == True:
+            self.params['wantgrad'] = False
+            
         self.wrapped_kernel = self.wrap_kernel(self.kernel, **self.params)
 
     def update_params(self, newparams_dict):
-        newparams_names = tuple(newparams_dict.keys())
-        assert len(self.kernel_arguments) == len(
+        newparams_names = list(newparams_dict.keys())
+        if not 'wantgrad' in newparams_names:
+            newparams_dict['wantgrad'] = False
+            newparams_names.append('wantgrad')
+
+            
+        assert len(self.find_arguments(self.kernel)) == len(
             newparams_names
         ), "wrong number of parameters for kernel!"
         assert not sum(
-            [not i in newparams_names for i in self.kernel_arguments]
+            [not i in newparams_names for i in self.find_arguments(self.kernel)]
         ), "you're trying to update a different list of parameters"
 
         self.params = newparams_dict
@@ -163,39 +171,98 @@ class GPR_reboot(object):
     # =============================================================================
     # > GAUSSIAN KERNEL
     @classmethod
-    def gaussian_kernel(cls, data1, data2, length=1, const=1):
+    def gaussian_kernel(cls, data1, data2, length=1, const=1, wantgrad=False):
+        returns = []
 
         square_diff = cls.difference_mat(data1, data2) ** 2
 
-        k = np.square(const) * np.exp(-2 * (square_diff / np.square(length)))
+        exp = np.exp(-2 * (square_diff / np.square(length)))
 
-        return k
+        k = np.square(const) * exp
+        returns.append(k)
+
+        if wantgrad == True:
+            dk_dc = 2 * const * exp
+            dk_dl = (np.square(const) * square_diff * exp) / (np.power(length, 3))
+
+            grads = {"length": dk_dl, "const": dk_dc}
+
+            returns.append(grads)
+
+        return np.squeeze(returns)
 
     # > PERIODIC KERNEL
     @classmethod
-    def periodic_kernel(cls, data1, data2, period=1, length=1, const=1):
+    def periodic_kernel(cls, data1, data2, period=1, length=1, const=1, wantgrad=False):
+        returns = []
 
         dist = cls.difference_mat(data1, data2)
-        
-        k = np.square(const) * np.exp(
-            -2 * np.square(np.sin((np.pi / period) * dist / np.square(length)))
-        )
+        sin_argument = (np.pi / period) * dist / np.square(length)
+        squared_sin = np.square(np.sin(sin_argument))
+        exponential = np.exp(-2 * squared_sin)
+        k = np.square(const) * exponential
+        returns.append(k)
 
-        return k
+        if wantgrad == True:
+            dk_dc = 2 * const * exponential
+            dk_dl = (
+                4 * np.square(const) * exponential * squared_sin / np.power(length, 3)
+            )
+            dk_dp = (
+                4
+                * np.pi
+                * np.square(const)
+                * exponential
+                * np.sin(sin_argument)
+                * np.cos(sin_argument)
+            ) / np.square(length * period)
+
+            grads = {"length": dk_dl, "period": dk_dp, "const": dk_dc}
+            returns.append(grads)
+
+        return np.squeeze(returns)
 
     # > RATIONAL QUADRATIC KERNEL
     @classmethod
-    def rational_quadratic(cls, data1, data2, alpha=1, length=1, const=1):
+    def rational_quadratic(
+        cls, data1, data2, alpha=1, length=1, const=1, wantgrad=False
+    ):
 
-        squared_diff = cls.difference_mat(data1, data2) ** 2
+        returns = []
 
-        k = np.square(const) * np.power(
-            (1 + squared_diff / (2 * alpha * np.square(length))), -alpha
-        )
+        diff = cls.difference_mat(data1, data2)
+        squared_diff = diff ** 2
 
-        return k
+        argument = 1 + squared_diff / (2 * alpha * np.square(length))
+        bracket = np.power(argument, -alpha)
 
-#    > MAUNA LOA KERNEL
+        k = np.square(const) * bracket
+        returns.append(k)
+
+        if wantgrad == True:
+            dk_dc = 2 * const * bracket
+            dk_dl = (
+                np.square(const)
+                * bracket
+                * squared_diff
+                / (np.power(length, 3) * argument)
+            )
+            dk_da = (
+                np.square(const)
+                * bracket
+                * (
+                    -np.log(argument)
+                    + (squared_diff / (2 * np.square(length) * alpha * argument))
+                )
+            )
+
+            grads = {"length": dk_dl, "const": dk_dc, "alpha": dk_da}
+
+            returns.append(grads)
+
+        return np.squeeze(returns)
+
+    #    > MAUNA LOA KERNEL
     @classmethod
     def mauna_loa_example_kernel(
         cls,
@@ -210,31 +277,47 @@ class GPR_reboot(object):
         RADQUAD_length=1,
         RADQUAD_shape=1,
         RBFnoise_length=1,
-        const=1,
+        RBFnoise_const=1,
+        wantgrad=False,
     ):
+        returns = []
 
-        gaussian_component = cls.gaussian_kernel(
-            data1=data1, data2=data2, length=RBF_length, const=RBF_const
+        gaussian_component, *grad_gaussiancomponent = cls.gaussian_kernel(
+            data1=data1, data2=data2, length=RBF_length, const=RBF_const, wantgrad=True
         )
 
-        periodic_component0 = cls.gaussian_kernel(
-            data1=data1, data2=data2, length=RBFperiodic_length, const=RBFperiodic_const
+        periodic_component0, *grad_periodic_component0 = cls.gaussian_kernel(
+            data1=data1,
+            data2=data2,
+            length=RBFperiodic_length,
+            const=RBFperiodic_const,
+            wantgrad=wantgrad,
         )
 
-        periodic_component1 = cls.periodic_kernel(
-            data1=data1, data2=data2, period=1, length=PERIODIC_length, const=1
+        periodic_component1, *grad_periodic_component1 = cls.periodic_kernel(
+            data1=data1,
+            data2=data2,
+            period=1,
+            length=PERIODIC_length,
+            const=1,
+            wantgrad=wantgrad,
         )
 
-        rational_quadratic_component = cls.rational_quadratic(
+        rational_quadratic_component, *grad_rational_quadratic_component = cls.rational_quadratic(
             data1=data1,
             data2=data2,
             alpha=RADQUAD_shape,
             length=RADQUAD_length,
             const=RADQUAD_const,
+            wantgrad=wantgrad,
         )
 
-        noise_component = cls.gaussian_kernel(
-            data1=data1, data2=data2, length=RBFnoise_length, const=const
+        noise_component, *grad_noise_component = cls.gaussian_kernel(
+            data1=data1,
+            data2=data2,
+            length=RBFnoise_length,
+            const=RBFnoise_const,
+            wantgrad=wantgrad,
         )
 
         k = (
@@ -244,7 +327,23 @@ class GPR_reboot(object):
             + noise_component
         )
 
-        return k
+        returns.append(k)
+
+        if wantgrad == True:
+            grads = {'RBF_const':           grad_gaussiancomponent['const'],
+                     'RBF_length':          grad_gaussiancomponent['length'],
+                     'RADQUAD_const':       grad_rational_quadratic_component['const'],
+                     'RADQUAD_length':      grad_rational_quadratic_component['length'],
+                     'RADQUAD_shape':       grad_rational_quadratic_component['alpha'],
+                     'RBFnoise_length':grad_noise_component['length'],
+                     'RBFnoise_const': grad_noise_component['const'],
+                     'RBFperiodic_const':   np.multiply(periodic_component1, grad_periodic_component0['const']),
+                     'RBFperiodic_length':  np.multiply(periodic_component1, grad_periodic_component0['length']),
+                     'PERIODIC_length':     np.multiply(periodic_component0, grad_periodic_component0['length'])}
+            
+            returns.append(grads)
+            
+        return np.squeeze(returns)
 
     # =============================================================================
     # K, K*, K** CALCULATIONS
@@ -270,7 +369,7 @@ class GPR_reboot(object):
         return self.wrapped_kernel(self.x, self.x)
 
     def calc_Ks(self):
-        return self.wrapped_kernel(self.x_guess, self.x)
+        return self.wrapped_kernel(self.x, self.x_guess)
 
     def calc_Kss(self):
         return self.wrapped_kernel(self.x_guess, self.x_guess)
@@ -285,11 +384,15 @@ class GPR_reboot(object):
     # =============================================================================
     # PREDICTIONS
     #        staticmethods
-    #            >calc_logp(alpha, L, y)ù
+    #            >calc_logp(alpha, L, y)
+    #            >calc_logp_nochol(Kn, Kn_inv, y, N)
+    #            >calc_gradlogp_i(Kinv, dK_di, y)
     #            > get_L_alpha(K,y)
-#                > get_Knoise(K,R,N)
+    #                > get_Knoise(K,R,N)
     #        instancemethods
     #            >calc_logp(self)
+    #            >predict(self)
+    #            >predict_nochol(self)
     # =============================================================================
     @staticmethod
     def calc_logp(alpha, L, y):
@@ -297,35 +400,88 @@ class GPR_reboot(object):
             -0.5 * np.dot(y.T, alpha) - np.trace(L) - 0.5 * len(y) * np.log(2 * np.pi)
         )
         return logp
-
-    @staticmethod
-    def get_L_alpha(K, y):
-        L = np.linalg.cholesky(K)
-        alpha = np.linalg.solve(L.T, np.linalg.solve(L, y))
-
-        return L, alpha
     
     @staticmethod
+    def calc_logp_nochol(Kn,Kn_inv,y,N):
+        logp = - 0.5 *  np.dot(y.T, np.dot(Kn_inv, y)) - 0.5 * np.log(np.trace(Kn)) - 0.5*N*np.log(2*np.pi)
+        return logp
+    
+    @staticmethod
+    def calc_gradlogp_i(Kinv, dK_di, y):
+        gradlogp_i = 0.5*np.dot(y.T,np.dot(Kinv, np.dot(dK_di,np.dot(Kinv, y)))) - 0.5*np.trace(np.dot(Kinv, dK_di))
+        
+        return gradlogp_i
+    
+    @staticmethod
+    def calc_gradlogp_i2(K, Kinv, dK, y):
+        alpha = np.dot(Kinv, y)
+        t = np.dot(alpha, alpha.T) - Kinv
+        grad = 0.5*np.trace(np.dot(t, dK))
+        
+        return grad
+    
+    @staticmethod
+    def get_L_alpha(Kn, y):
+        L = np.linalg.cholesky(Kn)
+        alpha = np.linalg.solve(L.T, np.linalg.solve(L, y))
+        return L, alpha
+
+    @staticmethod
     def get_Knoise(K, R, N):
-        return K + R*np.eye(N)
-
-    def predict(self):
-
+        return K + R * np.eye(N)
+    
+    
+    def predict(self, alt_logp = False):
+        
+        
         K_noise = self.get_Knoise(self.K, self.R, self.N)
-
+        
         L, alpha = self.get_L_alpha(K_noise, self.y)
-
+        
         self.logp = self.calc_logp(alpha, L, self.y)
 
         self.pred_y = np.dot(self.Ks.T, alpha)
+        
 
         v = np.linalg.solve(L, self.Ks)
+        
         self.pred_variance = np.diag(np.diag(self.Kss) - np.dot(v.T, v))
 
         if self.normalize_y == True:
             self.pred_y = self.pred_y + self.y_mean
 
         return self.pred_y, self.pred_variance, self.logp
+    
+    
+    def predict_nochol(self):
+        K_noise = self.get_Knoise(self.K, self.R, self.N)
+        
+        K_noise_inv = np.linalg.inv(K_noise)
+        
+        self.pred_y = np.dot(self.Ks.T, np.dot(K_noise_inv, self.y))
+        V = self.Kss - np.dot(self.Ks.T, np.dot(K_noise_inv, self.Ks))
+        self.pred_variance = np.diag(V)
+        
+        self.logp = self.calc_logp_nochol(K_noise, K_noise_inv, self.y, self.N)
+        
+        if self.normalize_y == True:
+            self.pred_y = self.pred_y + self.y_mean
+            
+        return self.pred_y, self.pred_variance, self.logp
+
+
+#    # TO BE REFINED TO MATCH OPTIMIZER REQUESTS
+#    def get_gradlogp(self, params):
+#        params['wantgrad'] = True
+#        
+#        K, gradK = self.calc_gradK(params)
+#        
+#        #FOR KEY IN DICT CALCULATE STUFF
+#        # but we don't really like dicts here....
+#        
+#        #RETURN STUFF
+#    
+        
 
     # =============================================================================
     # PLOTS
@@ -419,37 +575,82 @@ class GPR_reboot(object):
     # > OPTIMIZER
     # at the moment is a simple brute force optimizer
 
-    # TODO: implement a gradient optimizer
-    def optimizer(self, mode="brute", ranges_dict=None, Ns=100, output_grid=False):
 
-        param_names = tuple(ranges_dict.keys())
-        param_ranges = tuple(ranges_dict.values())
+    def optimizer(self, mode="brute", ranges_dict=None, param_x0 = None, Ns=100, output_grid=False):
 
-        modes = ["brute"]
+
+
+        modes = ["brute", 'CG','Newton-CG', 'Nelder-Mead']
         assert (
             mode in modes
         ), "please select a valid mode for the optimizer, choose between: {}".format(
             *modes
         )
-        returns = []
-
+        
         # NOT THE MOST EFFICIENT THING EVER
         def logp(x, *args):
-            params = x
+            params = np.exp(x)
             param_names = args
 
             param_dict = dict(zip(param_names, params))
+            param_dict['wantgrad'] = False
             w_kernel = self.wrap_kernel(self.kernel, **param_dict)
             try:
                 K = w_kernel(self.x, self.x)
-                L, alpha = self.get_L_alpha(K, self.y)
+                Kn = self.get_Knoise(K, self.R, self.N)
+                L, alpha = self.get_L_alpha(Kn, self.y)
                 logp = self.calc_logp(alpha, L, self.y)
+#                logp = self.alt_calc_logp(Kn, self.y, self.N)
+                
             except np.linalg.LinAlgError:
                 logp = -np.inf
-
+#                print("linalgerror")
+                
+#            print("logp: ", logp)
             return -logp
+        
+        def gradlogp(x, *args):
+            params = np.exp(x)
+            param_names = args
+            
+            grads = np.zeros(len(param_names))
 
+            param_dict = dict(zip(param_names, params))
+            
+            param_dict["wantgrad"] = True
+            
+            w_kernel = self.wrap_kernel(self.kernel, **param_dict)
+            K, gradK = w_kernel(self.x, self.x)
+            Kn = self.get_Knoise(K, self.R, self.N)
+            
+            for i, key in enumerate(param_names):
+                try:
+                    dK_di = gradK[key]
+                    dK_di_n = self.get_Knoise(dK_di, self.R, self.N)
+                    
+                    Kninv = np.linalg.inv(Kn)
+                    
+#                    grads[i] = -self.calc_gradlogp_i(Kninv, dK_di_n, self.y)
+                    
+                    grads[i] = - self.calc_gradlogp_i2(K, Kninv, dK_di_n, self.y)
+
+                except np.linalg.LinAlgError:
+#                    print("hey errore")
+                    grads[i] = np.inf
+
+#            print("grads: ", grads)
+            return grads
+            
+        def extract_results(minres, parm_names):
+            res = np.exp(minres.x)
+            logp = -minres.fun
+            return dict(zip(param_names, res)), logp
+        
         if mode == "brute":
+            returns  = []
+            param_names = tuple(ranges_dict.keys())
+            param_ranges = tuple(ranges_dict.values())
+            
             x0, fval, grid, Jout = brute_optim(
                 func=logp,
                 ranges=param_ranges,
@@ -466,5 +667,27 @@ class GPR_reboot(object):
             if output_grid:
                 returns.append(grid)
                 returns.append(Jout)
-
-        return returns
+                
+            return returns
+        
+        
+        elif mode in modes[1:]:
+            param_names = tuple(param_x0.keys())
+            param_x0 = tuple(param_x0.values())
+            
+            if mode == "CG" or mode == "Newton-CG":
+                jac = gradlogp
+            else:
+                jac = None
+            
+            
+            min_results = fmin(fun = logp,
+                           x0 = param_x0,
+                           args = (param_names),
+                           method = mode,
+                           jac = jac,
+                           options = {'disp': True},
+                           tol = 1e-3)
+            
+            return extract_results(min_results, param_names)
+            
